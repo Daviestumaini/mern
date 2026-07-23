43fa294007b0d985e5d323e324fe82b9742ae76c
Davies Tumaini <daviestumaini@gmail.com>
2026-07-23 11:08:27 +0300

    6

--- server/server.js
+++ server/server.js
@@ -17,8 +17,6 @@ app.use('/api/items', require('./routes/items'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
